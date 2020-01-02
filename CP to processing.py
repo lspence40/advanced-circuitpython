b75e97acd56a61839cc86bb833ea4aa59860e7ab
lspence40 <55842469+lspence40@users.noreply.github.com>
2020-01-01 20:26:20 -0500

    added comments

--- CP to processing.py
+++ CP to processing.py
@@ -3,14 +3,19 @@ from time import sleep
 from analogio import AnalogIn #pylint: disable-msg=import-error
 import busio #pylint: disable-msg=import-error
 
-analog_in = AnalogIn(board.A1)
+analog_in = AnalogIn(board.A1) #prepare to read the potentiometer
 
-def get_voltage(pin):
+def get_voltage(pin): #more preparation
     return (pin.value * 256) / 65536
 
 uart = busio.UART(board.TX, board.RX, baudrate=9600)
+#prepare to send data
 
-while True:
-    uart.write(bytes([int(get_voltage(analog_in))]))
-    print(bytes([int(get_voltage(analog_in))]))
-    sleep(.1)
+while True: #repeat forever
+    
+    uart.write(bytes([int(get_voltage(analog_in))])) #send data
+    
+    #print(bytes([int(get_voltage(analog_in))]))
+    #debugging
+    
+    sleep(.1) #do 10 times per second
